b96e3cb1ab131b5e45903c65f0a25b1772a4f830
Mokshith Voodarla <mvoodarla@gmail.com>
2018-11-24 19:29:17 -0800

    added further dampening to point turn at angle closer to goal

--- src/org/usfirst/frc/team2473/robot/RobotMap.java
+++ src/org/usfirst/frc/team2473/robot/RobotMap.java
@@ -16,5 +16,7 @@ public class RobotMap {
 	public static double K_TICKS_PER_INCH = 55;
 	public static double K_ENCODER_THRESHOLD = 1500;
 	public static double K_DEGREE_THRESHOLD = 70;
+	public static double K_DEGREE_THRESHOLD_CRITICAL = 30;
 	public static double K_ANGLE_DAMPEN = 0.95;
+	public static double K_ANGLE_DAMPEN_CRITICAL = 0.6;
 }
