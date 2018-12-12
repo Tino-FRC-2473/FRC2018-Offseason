801b550c168e6468966e605cdd9b0dfb49e4be15
Tejas Narayanan <tejasn100@gmail.com>
2018-12-11 20:56:05 -0800

    Successful command group testing with point turn and drive straight working pretty well

---
+++ src/org/usfirst/frc/team2473/robot/commands/AutonomousTester.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team2473.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+/**
+ *
+ */
+public class AutonomousTester extends CommandGroup {
+
+    public AutonomousTester() {
+    	double wait = 0.1;
+    	
+        /*
+        Lightning bolt
+        
+        addSequential(new StraightDrive(60, 0.5));
+        addSequential(new WaitCommand(wait));
+        
+        addSequential(new PointTurn(45, 0.45));
+        addSequential(new WaitCommand(wait));
+        
+        addSequential(new StraightDrive(30, 0.5));
+        addSequential(new WaitCommand(wait));
+        
+        addSequential(new PointTurn(-45, 0.45));
+        addSequential(new WaitCommand(wait));
+        
+        addSequential(new StraightDrive(30, 0.5));
+        
+         */
+        
+    	
+    	for (int i = 0; i < 4; i++) {
+    		addSequential(new StraightDrive(48, 0.5));
+            addSequential(new WaitCommand(wait));
+    		addSequential(new PointTurn(90, 0.45));
+            addSequential(new WaitCommand(wait));
+    	}
+    }
+}
