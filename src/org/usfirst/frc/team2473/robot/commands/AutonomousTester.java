2b0fa6cf5c14553d0b436afcf318d80398a3d615
Michelle <michellepan8@gmail.com>
2018-12-29 15:44:15 -0800

    Fix whitespace in AutonomousTester.java
    Indentation is now only with tabs instead of using both spaces and tabs.

--- src/org/usfirst/frc/team2473/robot/commands/AutonomousTester.java
+++ src/org/usfirst/frc/team2473/robot/commands/AutonomousTester.java
@@ -8,39 +8,37 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
  */
 public class AutonomousTester extends CommandGroup {
 
-    public AutonomousTester(int distanceFirst, int turnAngle, int distanceSecond) {
-    	double wait = 0.1;
+	public AutonomousTester(int distanceFirst, int turnAngle, int distanceSecond) {
+		
+		double wait = 0.1;
     	
+//		addSequential(new WaitCommand(wait));
+    	
+//		addSequential(new StraightDrive(60, 0.5));
+//		addSequential(new WaitCommand(wait));
         
-        //Lightning bolt
-//    	addSequential(new WaitCommand(wait));
-//    	
-//        addSequential(new StraightDrive(60, 0.5));
-//        addSequential(new WaitCommand(wait));
-//        
-//        addSequential(new PointTurn(45, 0.45));
-//        addSequential(new WaitCommand(wait));
-//        
-//        addSequential(new StraightDrive(30, 0.5));
-//        addSequential(new WaitCommand(wait));
-//        
-//        addSequential(new PointTurn(-45, 0.45));
-//        addSequential(new WaitCommand(wait));
-//        
-//        addSequential(new StraightDrive(30, 0.5));
+//		addSequential(new PointTurn(45, 0.45));
+//		addSequential(new WaitCommand(wait));
+        
+//		addSequential(new StraightDrive(30, 0.5));
+//		addSequential(new WaitCommand(wait));
+        
+//		addSequential(new PointTurn(-45, 0.45));
+//		addSequential(new WaitCommand(wait));
+        
+//		addSequential(new StraightDrive(30, 0.5));
 
-	      addSequential(new StraightDrive(distanceFirst, 0.3));
-	      addSequential(new WaitCommand(wait));
-	      addSequential(new PointTurn(turnAngle, 0.45));
-	      addSequential(new WaitCommand(wait));
-	      addSequential(new StraightDrive(distanceSecond, 0.3));
+		addSequential(new StraightDrive(distanceFirst, 0.3));
+		addSequential(new WaitCommand(wait));
+		addSequential(new PointTurn(turnAngle, 0.45));
+		addSequential(new WaitCommand(wait));
+		addSequential(new StraightDrive(distanceSecond, 0.3));
         
-    	
 //    	for (int i = 0; i < 4; i++) {
 //    		addSequential(new StraightDrive(48, 0.5));
-//            addSequential(new WaitCommand(wait));
+//    		addSequential(new WaitCommand(wait));
 //    		addSequential(new PointTurn(90, 0.45));
-//            addSequential(new WaitCommand(wait));
+//    		addSequential(new WaitCommand(wait));
 //    	}
     }
 }
